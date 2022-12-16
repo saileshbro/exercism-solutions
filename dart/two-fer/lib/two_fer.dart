34729fccabebe71fdcb1a2080cd264c503630935
Sailesh Dahal <saileshbro@gmail.com>
2022-12-16 10:40:53 +0545

    feat: add `two-fer`

---
+++ dart/two-fer/lib/two_fer.dart
@@ -0,0 +1,5 @@
+String twoFer([String? name]) {
+  if (name == 'Alice') return 'One for Alice, one for me.';
+  if (name == 'Bob') return 'One for Bob, one for me.';
+  return 'One for you, one for me.';
+}
